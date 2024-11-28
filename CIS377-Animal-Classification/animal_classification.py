d4c6f7ae57f2ac33b281e175daad315951f0fb59
Kaylee Odom <kaylee.odom@students.fhu.edu>
2024-11-27 19:54:26 -0600

    little fixes

--- CIS377-Animal-Classification/animal_classification.py
+++ CIS377-Animal-Classification/animal_classification.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.preprocessing.image import ImageDataGenerator # type: ignore
 
 # Translation dictionary for class labels
 TRANSLATE = {
